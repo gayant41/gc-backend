a4cedc7f4d97103dc3d61bf60f11fb134e0761ed
gayant41 <gayant41@gmail.com>
2025-08-13 13:09:46 +0530

    import cors

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ import User from "./modules/user.js";
 import productRouter from "./routes/productRouter.js";
 import orderRouter from "./routes/orderRouter.js";
 import dotenv from "dotenv";
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const mongoUrl = process.env.MONGODB_URL;
 
